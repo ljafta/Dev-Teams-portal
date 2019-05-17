d3ac3deb3d47fd1b82b4178a0839a201763e8030
Lumkile Jafta - BCX <lumkilej@bcx.co.za>
2019-05-17 13:03:42 +0200

    DevTeams

---
+++ EndToEnd/Models/DevContext.cs
@@ -0,0 +1,29 @@
+﻿using EndToEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity.Infrastructure;
+using System.Configuration;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EndToEnd.Models
+{
+
+    public partial class DevContext : DbContext
+    {
+        public DevContext()
+            : base("DefaultConnection")
+        // : base ("name=masterEntities")
+        {
+        }
+
+        public static DevContext Create()
+        {
+            return new DevContext();
+        }
+
+        public virtual DbSet<Groups> Groups { get; set; }
+    }
+}
---
+++ EndToEnd/Models/Groups.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EndToEnd.Models
+{
+    public class Groups
+    {
+        [Key]
+        public string Id { get; set; }
+        [Display(GroupName = "Group Name")]
+        public string GroupName { get; set; }
+    }
+}
